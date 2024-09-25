347b8e649b0817257ae09440f5c1c332617741a8
mandrantofit <mandrantofit@gmail.com>
2024-09-25 10:13:06 +0300

    modif ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'dev-stock-backend',    // Nom de l'application
+      name: 'backend',    // Nom de l'application
       script: './index.js',         // Chemin vers votre fichier principal
       instances: 1,             // Utiliser tous les cœurs du CPU disponibles
       exec_mode: 'fork',         // Mode cluster pour améliorer la performance
